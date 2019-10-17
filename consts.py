376349f638e889c0d37b30d555a437f2d2dc010c
fcs1 <47012319+fcs1@users.noreply.github.com>
2019-10-16 17:23:55 -0700

    Add files via upload

---
+++ consts.py
@@ -0,0 +1,8 @@
+
+
+
+table_names = {"tables" : []}
+table_meta_data = {}
+mem_table = {}
+post_function_types = ['Create Table', 'Insert Cell']
+get_function_types = ['List Table', 'Get Table Info']
